d28dde4add5f2f9d9354fac00a6ea6972a56c2e2
Travis Miller <radezorack@gmail.com>
2025-11-28 09:51:26 -0500

    checkpoint

--- jsos-dashboard/app/(components)/AixelPanel.tsx
+++ jsos-dashboard/app/(components)/AixelPanel.tsx
@@ -3,7 +3,7 @@ export default function AixelPanel() {
     <div className="fixed right-4 top-4 w-64 bg-white window-98 p-3 shadow-xl">
       <h2 className="font-bold mb-2">Aixel</h2>
       <p className="text-sm">
-        Your J’SOS companion.  
+        Your J’SOS companion asdadas.  
         (I'll become fully interactive later!)
       </p>
     </div>
